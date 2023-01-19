d627f4b7e4dff54fb09e2024100310530ae6dea1
Can Surer <cansurer@outlook.com>
2023-01-19 01:09:48 +0100

    crud work in json server complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/Booklist";
 
 function App() {
-  const [books, setBooks] = useState([
-    { id: 10, title: "Harry Potter" },
-    { id: 12, title: "Game Of Thrones" },
-    { id: 11, title: "Alice in Borderland" },
-  ]);
+  const [books, setBooks] = useState([]);
 
-  const deleteBookById = (id) => {
+
+  const fetchBooks = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/books");
+      setBooks(response.data);
+    } catch (error) {
+      console.error("err");
+    }
+  }
+
+  useEffect(()=> {
+    fetchBooks();
+
+  }, [])
+
+  const deleteBookById = async(id) => {
+
+  axios.delete(`http://localhost:3001/books/${id}`);
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
@@ -17,25 +31,28 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const editBookById = (id, newTitle) => {
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };
       }
 
       return book;
-    }); 
+    });
 
     setBooks(updatedBooks);
-
   };
 
-  const createBook = (title) => {
-    const updatedBooks = [
-      ...books,
-      { id: Math.round(Math.random() * 9999), title },
-    ];
-    setBooks(updatedBooks);
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    });
+    const updatedBooks = [...books, response.data];
+     setBooks(updatedBooks);
   };
   return (
     <div className="app">
